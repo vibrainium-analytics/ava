8ec2b6fbfd1293a7bbd50fea560a382ff82ce6c9
Spencer Harro <spencer.harro@gmail.com>
2017-02-23 08:56:34 -0500

    wrote basic plot page

---
+++ PlotPage.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+
+class PlotPage(tk.Frame):
+        def __init__(self, parent, controller):
+                tk.Frame.__init__(self, parent)
+                label = ttk.Label(self, text="Plot Page")
+                label.pack(pady=1,padx=1, side = "top", anchor = N)
+
+                goToHomePage_button = ttk.Button(self, text="Home",
+                                    command=lambda: controller.show_page("HomePage"))
+                goToHomePage_button.pack(pady=1,padx=15, side = LEFT, expand = NO, anchor = N)
