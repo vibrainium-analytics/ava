efbc387cfdb83739364371e0d5cbb3d487c19d30
Spencer Harro <spencer.harro@gmail.com>
2017-02-23 11:43:07 +0000

    Added FullScreenApp function

--- ava.py
+++ ava.py
@@ -33,7 +33,20 @@ a2 = f2.add_subplot(111)
 
 from sys import argv
 
-
+class FullScreenApp(object):
+    def __init__(self, master, **kwargs):
+        self.master=master
+        pad=3
+        self._geom='792x477+0+0'
+        master.geometry("{0}x{1}+0+0".format(
+        master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+        master.bind('<Escape>',self.toggle_geom)
+       
+    def toggle_geom(self,event):
+        geom=self.master.winfo_geometry()
+        print(geom,self._geom)
+        self.master.geometry(self._geom)
+        self._geom=geom
 class AVA(tk.Tk):    
     def __init__(self, *args,**kwargs):
         tk.Tk.__init__(self,*args,**kwargs)
