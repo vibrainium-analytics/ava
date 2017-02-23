cd096ca73d814af3134d3f41c20952b1c8bae2f5
Spencer Harro <spencer.harro@gmail.com>
2017-02-23 09:14:23 -0500

    Got basic app running

--- HomePage.py
+++ HomePage.py
@@ -2,9 +2,9 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 
-class StartPage(tk.Frame):
+class HomePage(tk.Frame):
 
-        def __init__(self,parent,controller)
+        def __init__(self,parent,controller):
                 tk.Frame.__init__(self,parent)
 
                 label = ttk.Label(self, text="Home Page")
@@ -15,9 +15,9 @@ class StartPage(tk.Frame):
                 goToRunTestPage_button.pack(pady=1, padx = 15, side = "left", expand = "no", anchor = "n")
 
                 goToPlotPage_button = ttk.Button(self, text="Plot",command=lambda: controller.show_page("PlotPage"))
-                dataCollectButton.pack(side = "right", expand = "yes", anchor = "n")
+                goToPlotPage_button.pack(side = "right", expand = "yes", anchor = "n")
 
-                goToNewVehiclePage_button = ttk.Button(self, text="New Car",
+                goToNewVehiclePage_button = ttk.Button(self, text="New Vehicle",
                                     command=lambda: controller.show_page("NewVehiclePage"))
                 goToNewVehiclePage_button.pack(side = "right", expand = "yes", anchor = "n")
 
--- RunTestPage.py
+++ RunTestPage.py
@@ -6,8 +6,8 @@ class RunTestPage(tk.Frame):
         def __init__(self, parent, controller):
                 tk.Frame.__init__(self, parent)
                 label = ttk.Label(self, text="Run Test Page")
-                label.pack(pady=1,padx=1, side = "top", anchor = N)
+                label.pack(pady=1,padx=1, side = "top", anchor = "n")
 
                 goToHomePage_button = ttk.Button(self, text="Home",
                                     command=lambda: controller.show_page("HomePage"))
-                goToHomePage_button.pack(pady=1,padx=15, side = LEFT, expand = NO, anchor = N)
+                goToHomePage_button.pack(pady=1,padx=15, side = "left", expand = "no", anchor = "n")
