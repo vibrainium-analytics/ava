8f46b8e49761ad04121bfb063af7a332df264cef
Spencer Harro <spencer.harro@gmail.com>
2017-02-25 00:37:44 +0000

    Added dropdown lists to RunTestPage

--- RunTestPage.py
+++ RunTestPage.py
@@ -17,3 +17,24 @@ class RunTestPage(tk.Frame):
                 goToHomePage_button = ttk.Button(self, text="Go Back",
                                     command=lambda: controller.show_page("HomePage"))
                 goToHomePage_button.pack(pady=1,padx=15, side = "left", expand = "no", anchor = "n")
+
+                Tests = ('Baseline - Idle', 'Baseline - 20 MPH', 'Baseline 30 MPH', 'Diagnotic - Idle', 'Diagnostic - 20 MPH')
+                TestType1 = ttk.Labelframe(self, text='Test Type')
+                TestType = ttk.Combobox(TestType1, values= Tests, state='readonly')
+                TestType.current(0)  # set selection
+                TestType.pack(pady=5, padx=10)
+                TestType1.pack(in_= self, side="top", pady=20, padx=10)
+
+                Delay = ('0 Seconds', '30 seconds', '60 seconds', '90 seconds', '120 seconds')
+                DelayTime1 = ttk.Labelframe(self, text='Delay before sampling begins')
+                DelayTime = ttk.Combobox(DelayTime1, values=Delay, state='readonly')
+                DelayTime.current(0)  # set selection
+                DelayTime.pack(pady=5, padx=10)
+                DelayTime1.pack(in_=self, side="top", pady=20, padx=10)
+
+                Duration = ('1 minute', '2 minutes', '5 minutes', '10 minutes', '20 minutes')
+                TestDuration1 = ttk.Labelframe(self, text='Sample size - time')
+                TestDuration = ttk.Combobox(TestDuration1, values=Duration, state='readonly')
+                TestDuration.current(0)  # set selection
+                TestDuration.pack(pady=5, padx=10)
+                TestDuration1.pack(in_=self, side="top", pady=20, padx=10)
