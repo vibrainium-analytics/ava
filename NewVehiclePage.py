eda81bb1ae84ffdd64f3b21b2fcdbfb4f03a3612
Spencer Harro <spencer.harro@gmail.com>
2017-02-25 00:40:41 +0000

    Added a dropdown to NewVehiclePage

--- NewVehiclePage.py
+++ NewVehiclePage.py
@@ -11,3 +11,10 @@ class NewVehiclePage(tk.Frame):
                 goToHomePage_button = ttk.Button(self, text="Home",
                                     command=lambda: controller.show_page("HomePage"))
                 goToHomePage_button.pack(pady=1,padx=15, side = "left", expand = "no", anchor = "n")
+
+                D = ('0 Seconds', '30 seconds', '60 seconds', '90 seconds', '120 seconds')
+                DT1 = ttk.Labelframe(self, text='Pre-defined - some will need this (cylinders) while other will need enterable fields')
+                DT = ttk.Combobox(DT1, values=D, state='readonly')
+                DT.current(0)  # set selection
+                DT.pack(pady=5, padx=10)
+                DT1.pack(in_=self, side="top", pady=20, padx=10)
