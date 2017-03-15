65e59c175560e8c901394da21ee864047ae116f0
sfreder <sdfreder@asu.edu>
2017-03-15 10:39:29 -0400

    Completed code for signal acquisition and processing. File creation is also complete. Wrote extensive comments to follow code.

--- Sample_Bar.py
+++ Sample_Bar.py
@@ -1,15 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
-import urllib.request
-import glob, os
-import json
-import shutil
-import math
+import os, json, shutil, math, urllib.request
 
 class Sample_Bar(tk.Tk):
 
     # create progress bar
-    
     def __init__(self,*args):
         tk.Tk.__init__(self,*args)
         self.progress = ttk.Progressbar(self, orient="horizontal", length=250, mode="determinate")
@@ -21,93 +16,106 @@ class Sample_Bar(tk.Tk):
         self.test()
         
     # update the progress
-    
     def updating(self,val):
         self.progress["value"] = val
 
-    # sample vibration data
-    
+    # sample vibration data 
     def test(self,i=0):   
-              
-        fname = "Three Axes"
-        xo = 2050                   # x-axis zero vibration value            
-        yo = 1605                   # y-axis zero vibration value
-        zo = 2060                   # z-axis zero vibration value 
+
+        # a function will be made here that uses the following values to determine the orientation of the sensor
+        # and remove the at rest values from the measured vibrations. This is done prior to the calculation of the
+        # vibration magnitude to remove noise from the frequency spectrum.
+        
+        xo = 2065                   # x-axis zero vibration no load
+        xup = 2475                  # x-axis zero vibration positive g
+        xdwn = 1650                 # x-axis zero vibration negative g
+        yo = 2020                   # y-axis zero vibration no load
+        yup = 2440                  # y-axis zero vibration positive g
+        ydwn = 1605                 # y-axis zero vibration negative g
+        zo = 2090                   # z-axis zero vibration no load
+        zup = 2510                  # z-axis zero vibration positive g
+        zdwn = 1675                 # z-axis zero vibration negative g
               
         # get test data from .json file
-        
         with open('data.json','r') as f:
             data = json.load(f)
-            f.close
-            
-        samples = 6 * (int(data['test_duration']))
+            f.close    
+
+        # The test duration is in minutes. The sample loop on the sensor module is 8 seconds plus 2 seconds for the progress bar to update
+        samples = 6 * (int(data['test_duration']))    
         testnm = str(data['test_type'])
 
-        # set directories from .json file
+        # get directories info from .json files
         with open('directory.json','r') as f:
             data_dir = json.load(f)
             f.close
-
         with open('data1.json','r') as f:
             data1 = json.load(f)
             f.close
-        
+
+        # set directories
         path = str(data_dir['veh_path'])
         path = path + str(data1['name']) + '_' + str(data1['make']) + '/' + testnm + '/'
         path1 = path + 'temp1/'
         path2 = path + 'temp/'
-
-        # for first loop i = 0 make directories only once.     
-        if i == 0:
-            os.makedirs(path1)
-            os.makedirs(path2) 
 
         # samples = 0 is a debug feature that bypasses sampling
         if samples == 0:
             self.destroy()
             print ('ok')
 
-        # if samples is anything other than zero the progress bar is updated
-        
+        # if samples is anything other than zero the progress bar is updated       
         else:
+
+            # for first loop i = 0 make directories only once remove them if they exist.
+            if i == 0:
+                if os.path.exists(path1):
+                    shutil.rmtree(path1)
+                os.makedirs(path1)
+                if os.path.exists(path2):
+                    shutil.rmtree(path2)
+                os.makedirs(path2)
+
+            # calculate amount of progress bar remaining    
             rem = i/samples
             self.updating(rem)
+
+            # if the progress bar is not filled sample vibration data from sensor module
             if i < samples:
+                name = 'Three Axes' + str(i+1)                                               
                 mkr = urllib.request.urlopen("http://192.168.1.1/A")
                 accl = mkr.read().decode()
                 mkr.close()
-                filenam = path1 + fname + '.txt'
-                f = open(filenam,"a")
+                filenam = path1 + name + '.txt'
+                f = open(filenam,"w")
                 f.write(accl)
-                f.close
-                self.after(2000, self.test, i+1)
-
-            # when sampling is done the progress bar goes away
-
+                f.close 
+                self.after(2000, self.test, i+1)        # 2 second delay to update progress bar
+                
+            # when sampling is done the magnitude of the vibrations is calculated and the 8 sample files are placed into one file
+            # the individual files are removed along with one of the temp folders
             elif i == samples:
-                self.destroy()
-                print('test is done')
-
-                # Calculate magnitude of the 3-axis vibrations and reduce DC component
-
-                fname1 = path1 + fname + '.txt'
-                f=open(fname1,'r')
-                data=f.readlines()
-                f.close
-                fname2 = path2 + fname + '.txt'
-                f=open(fname2,'a')
-                print (len(data))
-                for i in range(0, 4096):
-                    row = data[i]
-                    col = row.split()
-                    x = float(col[0]) - xo
-                    y = float(col[1]) - yo
-                    z = float(col[2]) - zo
-                    mag = int(math.sqrt(math.pow(x,2)+math.pow(y,2)+math.pow(z,2)))
-                    data1 = str(col[0]) + ' ' + str(col[1]) + ' ' + str(col[2]) + ' ' + str(mag) + '\n'
-                    f.write(data1)
+                accl = ''
+                for j in range (1, samples+1):
+                    name = 'Three Axes' + str(j)
+                    filenam = path1 + name + '.txt'
+                    f = open(filenam, 'r')
+                    data=f.readlines()
+                    f.close
+                    for i in range(0, 4096):
+                        row = data[i]
+                        col = row.split()
+                        x = float(col[0]) - xo
+                        y = float(col[1]) - ydwn
+                        z = float(col[2]) - zo
+                        mag = int(math.sqrt(math.pow(x,2)+math.pow(y,2)+math.pow(z,2)))
+                        a = str(col[0]) + ' ' + str(col[1]) + ' ' + str(col[2]) + ' ' + str(mag) + '\n'
+                        accl = accl + a
+                
+                fname2 = path2 + 'Three Axes' + '.txt'
+                f=open(fname2,'w')
+                f.write(accl)
                 f.close 
                 shutil.rmtree(path1)
-
-
-            
+                self.destroy()
+                print('test is done')
--- Signal_Process.py
+++ Signal_Process.py
@@ -1,14 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
-import glob, os
-import json
-import shutil
-import math
-import numpy
-
+from scipy import signal
+import os, json, shutil, math, numpy
 
 class Signal_Process(tk.Tk):
 
+    # create a message box. This will only display if the signal processing takes more than one second
     def __init__(self,*args):
         tk.Tk.__init__(self,*args)
         self.message = tk.Text(self, height=2, width=30)
@@ -17,62 +14,65 @@ class Signal_Process(tk.Tk):
         self.title('Processing')
         self.process()
 
+    # signal processing function
     def process(self):
             
-        # set directories using data from .json files
+        # get data from .json files
         with open('data.json','r') as f:
             data = json.load(f)
             f.close
-
         with open('data1.json','r') as f:
             data1 = json.load(f)
             f.close
-
         with open('data2.json','r') as f:
             data2 = json.load(f)
             f.close 
-
         with open('directory.json','r') as f:
             data_dir = json.load(f)
             f.close
 
+        # set AC status and speed status dependancies
         if str(data2['idle_status']) == 'Yes':
             testnm = '-Idle'
             if str(data2['ac_status']) == 'AC On':
                 testnm = testnm + '-AC'
         else:
             testnm = 'SteadySpeed-' + str(data2['speed'])
-            
+
+        # set directories using data from .json files    
         path = str(data_dir['veh_path'])
         path = path + str(data1['name']) + '_' + str(data1['make']) + '/' + str(data['test_type'])
         path1 = path + '/temp/'
         path2 = path + testnm + '/'
-        os.makedirs(path2)
+        if not os.path.exists(path2):
+            os.makedirs(path2)
         
-        # read magnitude values from all files into array
-
+        # read magnitude values into array and move raw data to new directory
         filename = path1 + 'Three Axes.txt'
+        filename2 = path2 + 'Three Axes.txt'
         f=open(filename,'r')
         data=f.readlines()
         f.close()
-        
-        m = numpy.zeros(len(data)-1)
+        shutil.move(filename,filename2)
+        shutil.rmtree(path)       
+        mag = numpy.zeros(len(data))
         for i in range(0, len(data)-1):
             row = data[i]        
             col = row.split()
-            m[i] = col[3]
-        mag = numpy.zeros(0)
-        mag = numpy.append(mag,m)
+            mag[i] = col[3]
             
-        # calculate running average of fft
-
-        n = 256
+        # calculate weighted average of fft
+        n = 256                                             # number of points in the FFT
         strt = 1
         fnsh = n
-        runav = 16
+        runav = 16                                          # number of FFT's in 8 second weighted average 
         smpl = mag[strt:fnsh]
+
+        # first FFT is outside the average loop so we aren't dividing by zero
         freq=numpy.absolute(numpy.fft.rfft(smpl))
         freq[0] = 0
+
+        # until we have done 16 FFT's it is a simple average
         for j in range(1, runav):
             strt = fnsh+1
             fnsh = fnsh+n
@@ -80,6 +80,8 @@ class Signal_Process(tk.Tk):
             freq1=numpy.absolute(numpy.fft.rfft(smpl))
             freq1[0] = 0
             freq = ((freq*j) + freq1)/(j+1)
+
+        # weighted average until end of samples
         while (fnsh+n) < len(mag):
             strt = fnsh+1
             fnsh = fnsh+n
@@ -93,7 +95,7 @@ class Signal_Process(tk.Tk):
         norm = s/(len(freq))
         freq = freq / norm
 
-        # write output to file
+        # write output to file with frequency scale
         filename2 = path2 + 'fft 250Hz.txt'
         for i in range(0, int(n/2)):
             hz = float("{0:.1f}".format(i * 500/n))
@@ -101,56 +103,118 @@ class Signal_Process(tk.Tk):
             with open(filename2, 'a') as out:
                 out.write(enrg)
         f.close
+
+        # create filter coefficients for zooming in by 2
+        b, a = signal.butter(20, .5)
+
+        # filter magnitude values that were read earlier
+        mag1 = signal.filtfilt(b, a, mag)
+
+        # downsample by 2 to avoid aliasing
+        stp = int(len(mag)/2)
+        mag = numpy.zeros(0)
+        for i in range(1, stp):
+            mag = numpy.append(mag,mag1[(2*i)-1])
+
+        # calculate weighted average of fft
+        n = 256                                                 # number of points in the FFT
+        strt = 1
+        fnsh = n
+        runav = 8                                               # number of FFT's in 8 second weighted average
+        smpl = mag[strt:fnsh]
+
+        # first FFT is outside the average loop so we aren't dividing by zero
+        freq=numpy.absolute(numpy.fft.rfft(smpl))
+        freq[0] = 0
+
+        # until we have done 8 FFT's it is a simple average
+        for j in range(1, runav):
+            strt = fnsh+1
+            fnsh = fnsh+n
+            smpl = mag[strt:fnsh]
+            freq1=numpy.absolute(numpy.fft.rfft(smpl))
+            freq1[0] = 0
+            freq = ((freq*j) + freq1)/(j+1)
+
+        # weighted average until end of samples
+        while (fnsh+n) < len(mag):
+            strt = fnsh+1
+            fnsh = fnsh+n
+            smpl = mag[strt:fnsh]
+            freq1=numpy.absolute(numpy.fft.rfft(smpl))
+            freq1[0] = 0
+            freq = ((freq*runav) + freq1)/(runav+1)
+
+        # normalize fft
+        s = numpy.sum(freq)
+        norm = s/(len(freq))
+        freq = freq / norm
+
+        # write output to file with frequency scale
+        filename2 = path2 + 'fft 125Hz.txt'
+        for i in range(0, int(n/2)):
+            hz = float("{0:.1f}".format(i * 250/n))
+            enrg = str(hz) + ' ' + str(float("{0:.2f}".format(freq[i]))) + '\n'
+            with open(filename2, 'a') as out:
+                out.write(enrg)
+        f.close
+
+        # create filter coefficients for zooming in by 2
+        b, a = signal.butter(20, .5)
+
+        # filter magnitude values from the previously filtered data
+        mag1 = signal.filtfilt(b, a, mag)
+
+        # downsample by 2 to avoid aliasing
+        stp = int(len(mag)/2)
+        mag = numpy.zeros(0)
+        for i in range(1, stp):
+            mag = numpy.append(mag,mag1[(2*i)-1])
+
+        # calculate weighted average of fft
+        n = 256                                                 # number of points in the FFT 
+        strt = 1
+        fnsh = n
+        runav = 4                                               # number of FFT's in 8 second weighted average
+        smpl = mag[strt:fnsh]
+
+        # first FFT is outside the average loop so we aren't dividing by zero
+        freq=numpy.absolute(numpy.fft.rfft(smpl))
+        freq[0] = 0
+
+        # until we have done 4 FFT's it is a simple average
+        for j in range(1, runav):
+            strt = fnsh+1
+            fnsh = fnsh+n
+            smpl = mag[strt:fnsh]
+            freq1=numpy.absolute(numpy.fft.rfft(smpl))
+            freq1[0] = 0
+            freq = ((freq*j) + freq1)/(j+1)
+
+        # weighted average until end of samples
+        while (fnsh+n) < len(mag):
+            strt = fnsh+1
+            fnsh = fnsh+n
+            smpl = mag[strt:fnsh]
+            freq1=numpy.absolute(numpy.fft.rfft(smpl))
+            freq1[0] = 0
+            freq = ((freq*runav) + freq1)/(runav+1)
+
+        # normalize fft
+        s = numpy.sum(freq)
+        norm = s/(len(freq))
+        freq = freq / norm
+
+        # write output to file with frequency scale
+        filename2 = path2 + 'fft 62.5Hz.txt'
+        for i in range(0, int(n/2)):
+            hz = float("{0:.1f}".format(i * 125/n))
+            enrg = str(hz) + ' ' + str(float("{0:.2f}".format(freq[i]))) + '\n'
+            with open(filename2, 'a') as out:
+                out.write(enrg)
+        f.close
         self.destroy()
 
-    
-##    freq = freq/2
-##    k=2
-##    while k < 4:
-##        b, a = signal.butter(20, .5)
-##        mag1 = signal.filtfilt(b, a, mag)
-##        stp = (int((len(mag)/k)-runav))
-##        mag = numpy.zeros(0)
-##        for i in range(1, stp):
-##            mag = numpy.append(mag,mag1[(2*i)-1])
-##        
-##        # calculate running average of fft
-##        strt = runav
-##        fnsh = n + runav-1
-##        smpl = mag[strt:fnsh]
-##        fq=numpy.absolute(numpy.fft.rfft(smpl))
-##        fq[0] = 0
-##        for j in range(1, runav):
-##            strt = fnsh+1
-##            fnsh = fnsh+n
-##            smpl = mag[strt:fnsh]
-##            fq1=numpy.absolute(numpy.fft.rfft(smpl))
-##            fq1[0] = 0
-##            fq = ((fq*j) + fq1)/(j+1)
-##        while (fnsh+n) < len(mag):
-##            strt = fnsh+1
-##            fnsh = fnsh+n
-##            smpl = mag[strt:fnsh]
-##            fq1=numpy.absolute(numpy.fft.rfft(smpl))
-##            fq1[0] = 0
-##            fq = ((fq*runav) + fq1)/(runav+1)
-##
-##        # normalize fft
-##        s = numpy.sum(fq)
-##        norm = s/(len(fq))
-##        fq = fq / norm  
-##
-##        # write output to file
-##        filename2 = datapath + 'fft' +str(freq/2) + 'Hz.txt'
-##        for i in range(0, int(n/2)):
-##            hz = float("{0:.1f}".format(i * freq/n))
-##            enrg = str(hz) + ' ' + str(float("{0:.2f}".format(fq[i]))) + '\n'
-##            with open(filename2, 'a') as out:
-##                out.write(enrg)
-##        f.close
-##        freq = freq/(2)
-##        k = k+1    
-##
 
 
                 
