83dc835f662d0e3ecb9606f1a6e16db80d6ef209
sfreder <sdfreder@asu.edu>
2017-04-12 12:30:15 -0400

    simplified and recalibrated mkr program

--- MKR/AVA_MKR.ino
+++ MKR/AVA_MKR.ino
@@ -8,22 +8,22 @@
   because it is a power of 2 and translates efficiently to a fast fourier transform for frequency 
   analysis.  Sampling for longer that 8 seconds is accomplished by using a loop in the parent program 
   allowing for a sample length of any multiple of 8.2 seconds. 
-  
-  The program can sample axes one at a time, or all three at once.  
+    
   Additional features include:
     Accelerometer self test feature 
     Time-out reset if no requests are recieved for 2.5 minutes
     Ten second delay that when used with a loop in the data acquisition program can be extended to 
       any multiple of ten seconds to allow vehicle to reach a desired state prior to sampling of 
       vibration data.
-
+      
   The sampled values are transmitted over a Wi-Fi connection to a local IP address 
   where the data can be viewed. For example:
     if the default IP address of your shield is http://192.168.1.1
-    http://192.168.1.1    Would display the menu screen.
-    http://192.168.1.1/A  Would send data from each of the 3 axes.
-    http://192.168.1.1/X  Would send data from the X axis only.  etc...
-
+    http://192.168.1.1    Display the menu screen.
+    http://192.168.1.1/A  Send data from each of the 3 axes.
+    http://192.168.1.1/D  Perform 10 second delay send message at beginng and end of 10 seconds.
+    http://192.168.1.1/S  Perform accelerometer self-test. Send test status message.
+    
 */
 
 #include <SPI.h> 
@@ -33,14 +33,11 @@ int led =  LED_BUILTIN;             // used to turn LED on
 int count = 0;                      // inactivity counter
 char ssid[] = "AVA-Sensor";         // wireless access point name
 String currentLine;                 // stores incomming data
-short cnt3 = 60;                    // number of times to run outer loop
-short cnt2 = 20;                    // number of times to run middle loop
-short cnt1 = 25;                    // number of times to run inner loop when sampling one axis
 short cnta = 8;                     // number of times to run inner loop sampling all 3 axes                         
-short datax[50];                    // holds x axis samples 
-short datay[50];                    // holds y axis samples
-short dataz[50];                    // holds z axis samples
-short r = 1810;                     // sample delay for sampling 3 axes 
+short datax[8];                    // holds x axis samples 
+short datay[8];                    // holds y axis samples
+short dataz[8];                    // holds z axis samples
+short r = 1782;                     // sample delay for sampling 3 axes 
 char nl = '\n';                     // new line character for formatting 
 char sp = ' ';                      // space character for formatting   
 int status = WL_IDLE_STATUS;
@@ -88,9 +85,6 @@ void loop() {
             client.print("Click <a href=\"/A\">here</a> to request data from all three axes <br>");
             client.print("Click <a href=\"/D\">here</a> to delay (10 seconds) <br>");
             client.print("Click <a href=\"/S\">here</a> to run accelerometer self test<br>");
-            client.print("Click <a href=\"/X\">here</a> to request x axis data<br>");
-            client.print("Click <a href=\"/Y\">here</a> to request y axis data<br>");
-            client.print("Click <a href=\"/Z\">here</a> to request z axis data<br>");
             
             client.println();                       // The HTTP response ends with a blank line:
             break;                                  // break out of the while loop
@@ -126,7 +120,7 @@ void loop() {
               dataz[1] = analogRead(A2); 
               int j = 1;                            // Initialize loop inner loop counter 
               int c1 = cnta;                        
-              while (j != c1){
+              while (j != c1-1){
                 j++;
                 delayMicroseconds(r);               // Delay to ensure sample every .002 seconds (500Hz)
                 datax[j] = analogRead(A0);
@@ -137,8 +131,8 @@ void loop() {
               // place integer data into string. cnta is chosen so that the transfer will take r microseconds
               
               j = 1;                                
-              while (j != cnta) {
-                csv = csv + String(datax[j])+ sp + String(datay[j]) + sp + String(dataz[j]) + sp3 + nl;
+              while (j != c1) {
+                csv = csv + String(datax[j])+ sp + String(datay[j]) + sp + String(dataz[j]) + nl;
                 j ++;
               }
               //read one more sample
@@ -188,74 +182,6 @@ void loop() {
           count = 0;                                // reset inactivity counter
           break;                                    
          }
-        
-        if (currentLine.endsWith("GET /X")) {
-          
-          // User requested data, Send header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-type:text/html");
-          client.println();
-          
-          short l = 0;                              // initialize outer loop counter
-          while (l != cnt3){                    
-            short k=0;                              // initialize mid loop counter
-            while (k != cnt2){  
-              String csv = "";                      // initialize to blank string
-              csv = data_read('X');                 // get results from data_read function
-              client.print(csv);                    // print the data to the IP address (this should also take q microseconds)
-              k++;                                  // increment the middle loop
-            }
-            l++;                                    // increment the outer loop
-          }
-          currentLine = "";                         // reset current line to prepare for next request
-          client.println();                         // The HTTP response ends with a blank line:
-          count = 0;                                // reset inactivity counter
-          break;                                    // break out of the while loop
-        }        
-
-        if (currentLine.endsWith("GET /Y")) {
-          // User requested data, Send header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-type:text/html");
-          client.println();
-          short l = 0;                              // initialize outer loop counter
-          while (l != cnt3){                    
-            short k=0;                              // initialize mid loop counter
-            while (k != cnt2){  
-              String csv = "";                      // initialize to blank string
-              csv = data_read('Y');                 // get results from data_read function
-              client.print(csv);                    // print the data to the IP address (this should also take q microseconds)
-              k++;                                  // increment the middle loop
-            }
-            l++;                                    // increment the outer loop
-          }
-          currentLine = "";                         // reset current line to prepare for next request
-          client.println();                         // The HTTP response ends with a blank line:
-          count = 0;                                // reset inactivity counter
-          break;                                    // break out of the while loop
-        }
-
-        if (currentLine.endsWith("GET /Z")) {
-          // User requested data, Send header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-type:text/html");
-          client.println();
-          short l = 0;                              // initialize outer loop counter
-          while (l != cnt3){                    
-            short k=0;                              // initialize mid loop counter
-            while (k != cnt2){  
-              String csv = "";                      // initialize to blank string
-              csv = data_read('Z');                 // get results from data_read function
-              client.print(csv);                    // print the data to the IP address (this should also take q microseconds)
-              k++;                                  // increment the middle loop
-            }
-            l++;                                    // increment the outer loop
-          }
-          currentLine = "";                         // reset current line to prepare for next request
-          client.println();                         // The HTTP response ends with a blank line:
-          count = 0;                                // reset inactivity counter
-          break;                                    // break out of the while loop
-        } 
       }
     }
 
@@ -268,55 +194,6 @@ void loop() {
   count++;
 }
 
-
-/* data_read function.  
- * 
- * Recieves the value of the axis to read, logs 25 samples, writes them to a string separated by 
- * new line characters, and returns the string. New line characters are selected for data 
- * formatting into a text file.
- * 
- * The data read and string conversion are done separately because the string converstion takes 
- * longer each time it is concatonated.  
- * By logging the data first the delay between samples is constant.
- */
- 
-String data_read(char axis) {
-  short pin;
-  short d = 1962;                         // Delay setting for 500Hz sample rate
-  String csv = "";
-  short data[25];
-  short j = 1;                            // Initialize loop inner loop counter (must be 1 to hold data in array position 1) 
-  short c1 = 24;                          // loop will log 24 samples (1 to 25)
-  // set pin to read
-  if (axis == 'X'){
-    pin = 0;                  
-  }
-  else if (axis == 'Y'){
-    pin = 1;
-  }
-  else if (axis == 'Z'){
-    pin = 2;
-  }
-  else {
-    csv = "error no axis selected";
-  }
-    // read first sample 
-  data[1] = analogRead(pin);  
-  while (j != c1){
-    j++;
-    delayMicroseconds(d);                 // Delay to ensure sample every .002 seconds (500Hz)
-    data[j] = analogRead(pin);            // Read data
-  }
-  j = 1;                                  // Use the same counter to transfer from array to string
-  while (j != c1+1) {
-    csv = csv + String(data[j])+ '\n';
-    j ++;
-  }
-  //read 25th sample and write it to the string
-  data[j] = analogRead(pin);
-  csv = csv + String(data[j]) + '\n';
-  return (csv);  
-}
 
 /* Self test function. 
  *  
@@ -370,5 +247,6 @@ String Self_test() {
   digitalWrite(11, 0);                      // turn off self test pin
   return(message);
 }
+
 
 
