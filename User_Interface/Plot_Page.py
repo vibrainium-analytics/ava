9c4524c94feece1af6183ca6087b340937785eca
sfreder <sdfreder@asu.edu>
2017-04-25 13:00:57 -0400

    Changes to normalization algorithm.  Labeled plot axes.

--- User_Interface/Plot_Page.py
+++ User_Interface/Plot_Page.py
@@ -29,8 +29,8 @@ matplotlib.use('TkAgg')
 fig = Figure(figsize=(6.7,3.5))
 a = fig.add_subplot(111)
 a.set_title("My Plot Title")
-a.set_xlabel("This is the X Axis")
-a.set_ylabel("This is the Y Axis")
+a.set_xlabel("Frequency Hz")
+a.set_ylabel("Normalized Energy")
 #fig.tight_layout()
 
 
@@ -60,8 +60,8 @@ def animate(i):
         # Create legend from plot label values
         a.legend()
 ##        a.set_title("My Plot Title")
-        a.set_xlabel("This is the X Axis")
-        a.set_ylabel("This is the Y Axis")
+        a.set_xlabel("Frequency Hz")
+        a.set_ylabel("Normalized Energy")
 
                  
 
